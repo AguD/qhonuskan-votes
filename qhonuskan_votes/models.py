cb6ed5bd5ba5867fbe04271889bb6be8b9ed81c1
Agustin Dahbar <agud.cba@gmail.com>
2018-10-23 21:24:56 -0300

    Django2.0 compatibility

--- qhonuskan_votes/models.py
+++ qhonuskan_votes/models.py
@@ -86,8 +86,8 @@ class VotesField(object):
             #__metaclass__ = VoteMeta
 
             voter = models.ForeignKey(
-                settings.AUTH_USER_MODEL,
-                verbose_name=_('voter'))
+                settings.AUTH_USER_MODEL, verbose_name=_('voter'),
+                on_delete=models.CASCADE)
 
             value = models.IntegerField(
                 default=1,
@@ -99,8 +99,7 @@ class VotesField(object):
                 verbose_name=_('voted on'))
 
             object = models.ForeignKey(
-                model,
-                verbose_name=_('object'))
+                model, verbose_name=_('object'), on_delete=models.CASCADE)
 
             class Meta:
                 ordering = ('date',)
